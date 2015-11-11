e9596ac81a0c563130462ac71bfe96dd01120001
griselgrisel <grisel.grisel@gmail.com>
2015-11-11 18:15:30 -0200

    Update tuga_std.py

--- src/tugalib/tuga_std.py
+++ src/tugalib/tuga_std.py
@@ -243,6 +243,7 @@ def invertido(x)
     
 def conjunto(x)
     '''
+    FABIO PRECISA DEFINIR ABORDAGEM
     
     Examples
     --------
@@ -251,17 +252,32 @@ def conjunto(x)
     
 def ordenado(x)
     '''
+    Ordena uma lista.
     
     Examples
     --------
+    
+    >>> sorted([5, 2, 3, 1, 4])
+    >>> [1, 2, 3, 4, 5]
     '''
     return sorted(x)
     
 def texto(x)
     '''
+ 
+    Create a new string object from the given object. If encoding or
+    errors is specified, then the object must expose a data buffer
+    that will be decoded using the given encoding and error handler.
+    Otherwise, returns the result of object.__str__() (if defined)
+    or repr(object).
+    encoding defaults to sys.getdefaultencoding().
+    errors defaults to 'strict'.
+    
+    Cria um novo texto a partir do texto fornecido.
     
     Examples
     --------
+    
     '''
     return str(x)
     
